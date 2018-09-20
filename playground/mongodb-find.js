9cf55d11ccd179f0ab3432da20f6d80a4d5b0a2e
Jason <jason@seasonsix.co>
2018-09-20 14:23:48 -0400

    Updated Files

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -23,5 +23,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         console.log('Unable to fetch todos', err);
     });
 
+    db.collection('Users').find({name: 'Jason Agyekum'}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    });
+
     // db.close();
 });
